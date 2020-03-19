9f415f0294dc74e03b41e0443512c361c1f307f1
Hector Castellanos <hectorcastellanos@Hectors-MacBook-Pro-2.local>
2020-03-18 18:33:09 -0700

    Added math program

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+a = 2
+b = 4
+c = a + b
+print(c) 
+d = 55
+e = d - c
+print(e)
+f = 72
+g = e * f
+print(g)
+h = 0
+i = g / h
+print(i)
---
+++ math.py
@@ -0,0 +1,11 @@
+# this program takes in two numbers and provides the result of adding, subtracting, multiplying and dividing them
+a = input ("please enter the first number: ")
+a = int(a)
+b = input ("please enter the second number: ")
+b = int(b)
+
+# this is showing me how to convert integer into a string and grouping them together
+print("Addition: " + str(a+b))
+print("Subtraction: " + str(a-b))
+print("Multiplication: " + str(a*b))
+print("Division: " + str(a/b))
